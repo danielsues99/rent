3dfc657dccf736382869d5558fa355d8a4cbb0a2
danielsuescun1520@gmail.com <danielsuescun1520@gmail.com>
2024-09-04 16:29:53 -0500

    feat: Adding auto-migration

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 RUN apt-get update && \
-    apt-get install -y netcat && \
+    apt-get install -y netcat gcc python3-dev libpq-dev && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
--- init_db.sh
+++ init_db.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
-# Waiting for DB
+export FLASK_APP=app.py 
+export FLASK_ENV=development  
+
 echo "Waiting for the availability of the DB..."
 while ! nc -z flask_db 5432; do
   sleep 1
 done
 
-echo "DB is up and running. Running migrations..."
+echo "DB is up and running."
 
+if [ ! -d "migrations" ]; then
+  echo "No migrations folder found. Initializing migrations..."
+  flask db init
+fi
+
+if [ ! -d "migrations/versions" ] || [ -z "$(ls -A migrations/versions)" ]; then
+  echo "No migration scripts found. Creating initial migration..."
+  flask db migrate -m "Initial migration"
+else
+  echo "Migration scripts found. Skipping migration creation."
+fi
+
+echo "Running database upgrade..."
 flask db upgrade
 
+if [ $? -ne 0 ]; then
+  echo "Migration failed. Check for errors."
+  exit 1
+fi
+
+echo "Starting Flask application..."
 exec flask run --host=0.0.0.0 --port=4000
